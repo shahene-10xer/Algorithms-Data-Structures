a8aa086e4742f1b8bb1019f416b7ecd222bce92c
shahene-10xer <124477831+shahene-10xer@users.noreply.github.com>
2023-03-22 11:38:30 -0700

    Time: 44 ms, Memory: 14.3 MB - LeetHub

--- search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array/search-in-rotated-sorted-array.py
@@ -1,21 +1,27 @@
 class Solution:
     def search(self, nums: List[int], target: int) -> int:
-        hashmap = {}
-        for i in range(len(nums)):
-            hashmap[nums[i]] = i
-        nums.sort()
-        print(nums)
         l, r = 0, len(nums) - 1
         while l <= r:
-            mid = l + (r - l) // 2
-            if nums[mid] < target:
-                l = mid + 1
-            elif nums[mid] > target:
-                r = mid - 1
-            elif nums[mid] == target:
-                return hashmap[nums[mid]]
-            else:
-                continue
+            mid = (l + r) // 2
+            if target == nums[mid]:
+                return mid
+        
+            # check if middle number is in left sorted portion
+            if nums[l] <= nums[mid]:
+                if target > nums[mid] or target < nums[l]:
+                    l = mid + 1
+                else:
+                    r = mid - 1
             
-        return -1 
-                
+            else:
+                if target < nums[mid] or target > nums[r]:
+                    r = mid - 1
+                else:
+                    l = mid + 1
+            
+        return -1
+        
+        
+        # Trivial solution would be O(n) time
+        # Solution must be O(log n) -> Figure out of mid is in left or right sorted portion
+        # Handle cases (use drawing)
